f3f90b5d360c1c9136745ccc333bb82cd9ca0bb9
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-07-12 20:31:51 -0700

    commit tree dbfbc3d7e8d023e7ade91190420499457c880418

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Article.create(title: 'Interesting Article', description: 'A really interesting article')
+Article.create(title: 'Boring Article', description: 'A really boring article')
